cea7b744c0766b9729023a0c81e3278d597579df
thompa21 <tholind@kth.se>
2023-06-13 14:44:14 +0200

    roomsavailability

--- Models.js
+++ Models.js
@@ -68,7 +68,8 @@ const readBookingsForHour = (system, room_id, timestamp) => {
                      AND mrbs_entry.end_time > ?`;
         params = [room_id, timestamp, timestamp]
 
-        console.log(database.format(query, params));
+        console.log(query);
+        console.log(params);
         connection.query(query, params, (err, results, fields) => {
             if (err) {
               console.error('Error executing query:', err);
